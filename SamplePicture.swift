47ed0ad68eaccd14719882e43ca28fa59d69b282
colesda <colesda@onid.oregonstate.edu>
2015-01-26 12:56:04 -0800

    1/26/2015
    See ChangeLog

--- SamplePicture.swift
+++ SamplePicture.swift
@@ -10,15 +10,69 @@
 import UIKit
 import AVFoundation
 
-
-//Create a struct containing pixel color data
 struct RGB_Pixel_Data {
     var red    = 0
     var green  = 0
     var blue   = 0
 }
 
+
+
+
+/*extension UIImage {
+    func convertRGBAtoHSV(RGB_Pixel_Data){
+        struct convertRGBAtoHSV hsv
+        
+        CGFloat rgb_min, rgb_max
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+}
+
+/*
++(struct hsv_color)HSVfromRGB:(struct rgb_color)rgb
+{
+struct hsv_color hsv;
+
+CGFloat rgb_min, rgb_max;
+rgb_min = MIN3(rgb.r, rgb.g, rgb.b);
+rgb_max = MAX3(rgb.r, rgb.g, rgb.b);
+
+if (rgb_max == rgb_min) {
+hsv.hue = 0;
+} else if (rgb_max == rgb.r) {
+hsv.hue = 60.0f * ((rgb.g - rgb.b) / (rgb_max - rgb_min));
+hsv.hue = fmodf(hsv.hue, 360.0f);
+} else if (rgb_max == rgb.g) {
+hsv.hue = 60.0f * ((rgb.b - rgb.r) / (rgb_max - rgb_min)) + 120.0f;
+} else if (rgb_max == rgb.b) {
+hsv.hue = 60.0f * ((rgb.r - rgb.g) / (rgb_max - rgb_min)) + 240.0f;
+}
+hsv.val = rgb_max;
+if (rgb_max == 0) {
+hsv.sat = 0;
+} else {
+hsv.sat = 1.0 - (rgb_min / rgb_max);
+}
+
+return hsv;
+}
+*/
+
+*/
+
+
+
+
 class SamplePicture: UIViewController {
+
+
     /*
     var sample_image = UIImage(named: "BlackSample")
     
@@ -32,6 +86,32 @@ class SamplePicture: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let green = UIImage(named: "BlackSample.png")
+        
+        
+        
+        
+        //let image1 = UIImage(named: "BlackSample")
+        
+        let topLeft = CGPoint(x: 0, y: 0)
+        
+        // Use your extension
+        let greenColour = green?.getPixelColor(topLeft)
+        
+        // Dump RGBA values
+        var redval: CGFloat = 0
+        var greenval: CGFloat = 0
+        var blueval: CGFloat = 0
+        var alphaval: CGFloat = 0
+        greenColour?.getRed(&redval, green: &greenval, blue: &blueval, alpha: &alphaval)
+            //getRed(&redval, green: &greenval, blue: &blueval, alpha: &alphaval)
+        
+        println("Green is r: \(redval) g: \(greenval) b: \(blueval) a: \(alphaval)")
+        
+        
+        
+        
+        
     }
     
     
---
+++ project_26_SP/RGBA_Converter  .swift
@@ -0,0 +1,19 @@
+import UIKit
+import AVFoundation
+
+extension UIImage {
+    func getPixelColor(pos: CGPoint) -> UIColor {
+        
+        var pixelData = CGDataProviderCopyData(CGImageGetDataProvider(self.CGImage))
+        var data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        
+        var pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+        
+        var r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+        var g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+        var b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+        var a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+        
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+    }
+}
