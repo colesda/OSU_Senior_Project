2b462b5204247d390d05bc56cf7645c3cd796031
davidcoles88 <admin@10-162-169-44.wireless.oregonstate.edu>
2015-02-01 08:42:55 -0800

    Test Commit
    Test

--- project_26_SP/RGBA_Converter  .swift
+++ project_26_SP/RGBA_Converter  .swift
@@ -15,5 +15,10 @@ extension UIImage {
         var a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
         
         return UIColor(red: r, green: g, blue: b, alpha: a)
+        
+        //test
+        
+        
+        
     }
 }
