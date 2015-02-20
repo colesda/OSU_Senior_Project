2f083447eb8a482e9da12b6291dc56661af13c24
vocosu <voc@onid.oregonstate.edu>
2015-02-19 22:13:45 -0800

    Camera Test now brings up new camera functionality
    New camera functionality takes picture every 5 seconds on a 30 second
    timer. Can change from front and back camera and also takes a video.
    Saves to the iOS camera roll.

---
+++ project_26_SP/AVCamPreviewView.swift
@@ -0,0 +1,28 @@
+//
+//  AVCamPreviewView.swift
+//  project_26_SP
+//
+//  Created by Chynh Vo on 2/19/15.
+//  Copyright (c) 2015 Remcho Research. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+
+class AVCamPreviewView: UIView {
+    
+    var session: AVCaptureSession? {
+        get{
+            return (self.layer as AVCaptureVideoPreviewLayer).session;
+        }
+        set(session){
+            (self.layer as AVCaptureVideoPreviewLayer).session = session;
+        }
+    };
+    
+    override class func layerClass() ->AnyClass{
+        return AVCaptureVideoPreviewLayer.self;
+    }
+}
